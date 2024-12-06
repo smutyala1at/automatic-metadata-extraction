e679cf0575120da03824afa35dfec300621a51ad
Santosh Mutyala <santosh.mutyala@uni-oldenburg.de>
2024-12-06 21:46:04 +0100

    add new key to filter readmes

--- utils.py
+++ utils.py
@@ -32,8 +32,43 @@ def get_token_count(input_file, output_file):
         json.dump(readmes, wf, indent=4)
 
 
-input_file="./files/readmes.json"
-output_file="./files/readmes_output.json"
+def filter_readmes(input_file, output_file):
 
-get_token_count(input_file, output_file)
-get_readme_file_length("./files/readmes.json")
+    with open(input_file, "r", encoding="utf-8") as rf:
+        readmes = json.load(rf)
+
+        # check if the input files contains a list of objects
+        if isinstance(readmes, list):
+            for obj in readmes:
+                if obj["token_count"] > 2000:
+                    obj["process_to_llm"] = False
+                else:
+                    obj["process_to_llm"] = True
+        else:
+            raise ValueError("The input file must contain a list of objects")
+        
+        with open(output_file, "w", encoding="utf-8") as wf:
+            json.dump(readmes, wf, indent=4)
+
+
+def get_valid_readmes_count(input_file):
+
+    with open(input_file, "r", encoding="utf-8") as rf:
+        readmes = json.load(rf)
+        print("total readmes count ", len(readmes))
+
+        if isinstance(readmes, list):
+            count = 0
+            for obj in readmes:
+                if obj["process_to_llm"] == True:
+                    count += 1
+            print("allowed readmes count: ", count)
+        else:
+            raise ValueError("The input file must contain a list of objs")
+
+
+# get_token_count("./files/readmes.json", "./files/readmes_output.json")
+# get_readme_file_length("./files/readmes.json")
+
+filter_readmes("readmes_output.json", "filtered_readmes.json")
+get_valid_readmes_count("filtered_readmes.json")
